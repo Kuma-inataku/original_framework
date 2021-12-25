2bec84b66e00f61cd3e178dbf4b5811ec4ce1c64
Kuma-inataku <inataku0121@gmail,com>
2021-12-25 09:27:16 +0900

    ログイン処理の制御

--- core/Application.php
+++ core/Application.php
@@ -7,6 +7,7 @@ abstract class Application
   protected $response;
   protected $session;
   protected $db_manager;
+  protected $login_action = [];
 
   public function __construct($debug = false)
   {
@@ -104,6 +105,9 @@ abstract class Application
       $this->runAction($controller, $action, $params);
     } catch (HttpNotFoundException $e) {
       $this->render404Page($e);
+    } catch (UnauthorizedActionException $e) {
+      list($controller, $action) = $this->login_action;
+      $this->runAction($controller, $action, $params);
     }
 
     $this->response->send();
